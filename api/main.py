ff9c569e1da88ce424f1ca2fdf4bed7e87ce2386
Radiohead229 <rajnayanmir@gmail.com>
2025-05-18 15:08:50 +0530

    model epoch update

--- api/main.py
+++ api/main.py
@@ -21,7 +21,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-MODEL = tf.keras.models.load_model("../saved_models/5/5.keras")
+MODEL = tf.keras.models.load_model("../saved_models/1/1.keras")
 
 CLASS_NAMES = ["Early Blight", "Late Blight", "Healthy"]
 
